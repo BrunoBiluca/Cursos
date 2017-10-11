61f17ea3d3ad7c9da89dd03e6772b60233c3fab2
Bruno Bernardes da Costa <b.b.da.costa@gmail.com>
2017-10-11 07:46:20 -0300

    Implementação do padrão FlyWeight na Unity

---
+++ DesignPatterns/FlyWeightPattern/Exemplo_FlyWeight_Unity/Assets/Alien.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets {
+    public class Alien {
+        public List<Vector3> EyePositions;
+        public List<Vector3> LegPositions;
+        public List<Vector3> ArmPositions;
+    }
+}
---
+++ DesignPatterns/FlyWeightPattern/Exemplo_FlyWeight_Unity/Assets/FlyWeight.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets {
+    class FlyWeight : MonoBehaviour {
+
+        List<Alien> AllAliens = new List<Alien>();
+
+        /// <summary>
+        /// Objetos que serão compartilhados
+        /// </summary>
+        List<Vector3> EyePositions; 
+        List<Vector3> LegPositions;
+        List<Vector3> ArmPositions;
+
+        /// <summary>
+        /// Devemos reparar no uso total de memória que cai de 0.69Gb para 176Mb
+        /// Principalmente a informação do "Mono" que é o tamanho total do heap que é utilizado
+        /// </summary>
+        void Start() {
+            EyePositions = GetBodyPartPositions();
+            LegPositions = GetBodyPartPositions();
+            ArmPositions = GetBodyPartPositions();
+            
+            //Create all aliens
+            for (int i = 0; i < 10000; i++) {
+                Alien newAlien = new Alien();
+
+                //Without flyweight
+                newAlien.EyePositions = GetBodyPartPositions();
+                newAlien.ArmPositions = GetBodyPartPositions();
+                newAlien.LegPositions = GetBodyPartPositions();
+
+                //With flyweight
+                newAlien.EyePositions = EyePositions;
+                newAlien.ArmPositions = LegPositions;
+                newAlien.LegPositions = ArmPositions;
+
+                AllAliens.Add(newAlien);
+            }
+        }
+
+        private List<Vector3> GetBodyPartPositions() {
+            List<Vector3> bodyPartPositions = new List<Vector3>();
+
+            // Simula um objeto com 1000 pontos
+            for (int i = 0; i < 1000; i++) {
+                bodyPartPositions.Add(new Vector3());
+            }
+
+            return bodyPartPositions;
+        }
+    }
+}

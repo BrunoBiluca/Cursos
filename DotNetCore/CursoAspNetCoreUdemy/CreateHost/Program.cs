7aa483b0c84a02ef15cab880d4b60b05b7179641
Bruno Bernardes da Costa <b.b.da.costa@gmail.com>
2018-01-18 23:43:17 -0200

    Criação de host no AspNetCore

---
+++ DotNetCore/CursoAspNetCoreUdemy/CreateHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace CreateHost {
+    class Program {
+        static void Main(string[] args) {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => {
+                    app.Run(context => context.Response.WriteAsync("Bão demais na mão"));
+                })
+                .Build();
+            host.Run();
+        }
+    }
+}

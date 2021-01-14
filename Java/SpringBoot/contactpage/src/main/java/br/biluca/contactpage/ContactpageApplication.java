d1d2c115d274dc000cb1429e9d2e237b744a0352
brunobiluca <b.b.da.costa@gmail.com>
2021-01-14 17:18:35 -0300

    :sparkles: Criação de página de contato utilizando Spring Boot, Java 15, Thymeleaf e H2

---
+++ Java/SpringBoot/contactpage/src/main/java/br/biluca/contactpage/ContactpageApplication.java
@@ -0,0 +1,13 @@
+package br.biluca.contactpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactpageApplication.class, args);
+	}
+
+}
---
+++ Java/SpringBoot/contactpage/src/test/java/br/biluca/contactpage/ContactpageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.biluca.contactpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

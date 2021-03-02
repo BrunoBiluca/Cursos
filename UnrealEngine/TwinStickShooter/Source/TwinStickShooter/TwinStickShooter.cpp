8f019786dfdeb33bdaf3734c7ef19fb9589375ae
brunobiluca <b.b.da.costa@gmail.com>
2021-03-02 11:43:29 -0300

    :fire: Remoção do código de Twin Stick Shooter para um repositório próprio

--- UnrealEngine/TwinStickShooter/Source/TwinStickShooter/TwinStickShooter.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TwinStickShooter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickShooter, "TwinStickShooter" );
